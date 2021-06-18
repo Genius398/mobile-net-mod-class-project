5fb24716449e2be88910eed034808549737b4c72
Anantjit <anantjitchander@gmail.com>
2021-06-18 19:06:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 }
 
+function draw(){
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
 
+function modelLoaded(){
+  console.log('Model Loaded');
+}
+
+function gotResult(error, results){
+  if (error)
+  {
+    console.error(error);
+  }
+  else
+  {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
